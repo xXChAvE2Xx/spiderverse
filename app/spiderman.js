3f65dd178634772890c9c81b7f372137027a5910
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-04-22 18:23:48 -0500

    Agregando clase de spiderman

---
+++ app/spiderman.js
@@ -0,0 +1,15 @@
+class Spiderman{
+    constructor(name, age, actor, movies, studio){
+        this.name = name;
+        this.age = age;
+        this.actor = actor;
+        this.movies = movies;
+        this.studio = studio;
+    }
+
+    getInfo(){
+        return `Hey, I'm ${this.actor} from ${this.studio}`
+    }
+}
+
+module.exports = Spiderman;
